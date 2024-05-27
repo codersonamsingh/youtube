425ea342bcb6d5323802fb2f06a7f2129a46401f
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-05-27 22:39:17 +0530

    navbar running

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
+
+
+const Navbar = () => {
+    return (
+        <nav className='flex-div'>
+            <div className='nav-left flex div '>
+                <img className='menu_icon' src={menu_icon} alt="" />
+                <img className= 'logo' src={logo} alt="" />
+
+            </div>
+        </nav>
+    )
+}
+export default Navbar
